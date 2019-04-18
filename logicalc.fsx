36452d1c683b4b8a8be4ed340dca3d45c42c2dca
wildart <wildart@gmail.com>
2019-04-18 15:41:48 -0400

    added test for an evaluation result

--- logicalc.fsx
+++ logicalc.fsx
@@ -6,76 +6,83 @@ open Parser
 #load "build/logicalc-lex.fs"
 open Lexer
 
-let parse str =
+let parse str result =
     printfn "Evaluate:\n %s" str
     let lexbuf = LexBuffer<char>.FromString str
-    Parser.start Lexer.read lexbuf
+    if (Parser.start Lexer.read lexbuf) = result
+    then printfn "Expression is correct."
+    else failwithf "Expression interpreted incorrectly. Expression must be evaluated to `%b`." (not result)
+
 
 // And Operation
-parse "F & T\n"
+parse "F & T\n" false
 
 // Implication and Inference
-parse "F => T |- T\n"
+parse "F => T |- T\n" true
 
 // Variabes
 parse """
     a = T
     a & F
-"""
+""" false
 
 // Modus Ponens
 parse """
     p = T
     q = F
     (p => q) & p |- q
-"""
+""" true
 
 // Modus Tollens
 parse """
     p = F
     q = F
     (p => q) & !q |- !p
-"""
+""" true
 
 // Addition
 parse """
     p = T
     q = F
     p |- (p | q)
-"""
+""" true
 
 // Material Equivalence
 parse """
     p = T
     q = F
     p <=> q |- (p => q) & (q => p)
-"""
+""" true
 
 // Conditional Expression
 parse """
     (if F then F else T)
-"""
+""" true
 
 // Conditional Expression
 parse """
     p = T
     q = (if p then F else T)
     r = (if p then F)
+    q => r
+""" true
+
+parse """
     p = F
     q = (if p then F else T)
     r = (if p then F)
-    p
-"""
+    q & r
+""" false
 
 // Numerical literals and equality
 parse """
     (1 == 1)
-"""
+""" true
 
 // Other relational operations
 parse """
     (1113 > 1) & (19823 != 1)
-"""
+""" true
 
 printfn "\nALL TESTS PASSED!\n\nPress any key to continue..."
 System.Console.ReadLine() |> ignore
